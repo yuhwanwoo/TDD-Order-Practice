03138220f5b7435804cc3dfe5657581681a44bf5
yuhwanwoo <6514687@naver.com>
2024-01-10 22:46:42 +0900

    [tdd] 상품 조회 api

--- src/test/java/com/tdd/tddorderpractice/product/ProductApiTest.java
+++ src/test/java/com/tdd/tddorderpractice/product/ProductApiTest.java
@@ -1,6 +1,7 @@
 package com.tdd.tddorderpractice.product;
 
 import com.tdd.tddorderpractice.ApiTest;
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,21 @@ class ProductApiTest extends ApiTest {
         final ExtractableResponse<Response> response = ProductSteps.상품등록요청(request);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 상품조회() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
     }
 
 }
