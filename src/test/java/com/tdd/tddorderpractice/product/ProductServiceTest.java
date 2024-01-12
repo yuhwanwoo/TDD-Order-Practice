9a7acb9caaec7711d0c9c0d779ff0edcfa23b45a
yuhwanwoo <6514687@naver.com>
2024-01-12 10:38:14 +0900

    [tdd] springboot 테스트 변경

--- src/test/java/com/tdd/tddorderpractice/product/ProductServiceTest.java
+++ src/test/java/com/tdd/tddorderpractice/product/ProductServiceTest.java
@@ -1,18 +1,19 @@
 package com.tdd.tddorderpractice.product;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
+    @Autowired
     private ProductPort productPort;
 
     @BeforeEach
@@ -23,10 +24,10 @@ public class ProductServiceTest {
 
     @Test
     void 상품수정() {
+        productService.addProduct(ProductSteps.상품등록요청_생성());
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
         final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         productService.updateProduct(productId, request);
 
