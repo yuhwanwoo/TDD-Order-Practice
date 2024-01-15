01f6f47c40bd32d08c075e38ad43726bb6a8ea80
yuhwanwoo <6514687@naver.com>
2024-01-16 07:55:50 +0900

    [tdd] api 전환 및 jpa 적용

---
+++ src/test/java/com/tdd/tddorderpractice/order/OrderApiTest.java
@@ -0,0 +1,25 @@
+package com.tdd.tddorderpractice.order;
+
+import com.tdd.tddorderpractice.ApiTest;
+import com.tdd.tddorderpractice.product.ProductSteps;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrderApiTest extends ApiTest {
+
+
+    @Test
+    void 상품주문() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        final CreateOrderRequest request = OrderSteps.상품주문요청_생성();
+
+        final ExtractableResponse<Response> response = OrderSteps.상품주문요청(request);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+}
